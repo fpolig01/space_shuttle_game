be20a2f97242ca01866018add78754a36acacdf0
Fran <fpolig01@villanova.edu>
2017-10-04 10:05:58 -0400

    long way of doing text display

--- space_shuttle_game.py
+++ space_shuttle_game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,time
 
 pygame.init()
 
@@ -9,49 +9,85 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
+car_width = 105
+
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption("Franman's Game")
+pygame.display.set_caption("Frans Space Game")
 clock = pygame.time.Clock()
 
 space_shuttle_img = pygame.image.load('space_shuttle_3_alpha.png')
-
-
-x = display_width * 0.45
-y = display_height * 0.7
-
-x_change = 0
 
 
 def shuttle(x, y):
     gameDisplay.blit(space_shuttle_img, (x, y))
 
 
-crashed = False
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
-while not crashed:
 
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.QUIT:
-            crashed = True
+def message_display(text):
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
+    TextSurf, TextRect = text_objects(text, largeText)
 
-    x += x_change
-    print(x)
 
-    gameDisplay.fill(white)
-    shuttle(x, y)
+    TextRect.center = ((display_width/2), (display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
     pygame.display.update()
-    clock.tick(60)
+    time.sleep(2)
 
+    game_loop()
+
+
+def crash():
+
+    message_display('You crashed')
+
+
+def game_loop():
+
+    x = display_width * 0.45
+    y = display_height * 0.7
+
+    x_change = 0
+
+    game_exit = False
+
+    while not game_exit:
+
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+        print(x)
+
+        gameDisplay.fill(white)
+        shuttle(x, y)
+
+        if x > display_width - car_width or x < 0:
+            crash()
+
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+game_loop()
 pygame.quit()
 quit()
